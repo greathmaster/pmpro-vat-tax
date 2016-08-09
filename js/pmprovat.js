e7940d0ef3c1e2811fb6074600c5bf153d18d1b2
greathmaster <counthmaster@yahoo.com>
2016-08-09 08:55:03 -0700

    ENHANCEMENT: Added Geo IP support and EU VAT data to Orders

--- js/pmprovat.js
+++ js/pmprovat.js
@@ -1,15 +1,34 @@
 jQuery(document).ready(function(){
+	jQuery("#reg_price").hide();
+	
+	vat_number_verified = get_query_var('vat_number_verified');
+	
+	if(vat_number_verified == 1)
+	{
+		jQuery("#reg_price").show();
+		jQuery("#vat_price").hide();
+	}
+	
 	jQuery("select[name='bcountry']").change(function() {
-		var country = jQuery('select[name="bcountry"] option:selected').val();		
+			
+		var country = jQuery('select[name="bcountry"] option:selected').val();
+		var country_by_ip = jQuery('#geo_ip').val();
 		var showHideVATTable;
 		showHideVATTable = jQuery.inArray(country, pmprovat.eu_array);
 				
-		if(showHideVATTable > -1)
+		if(showHideVATTable > -1 && vat_number_verified != 1)
 		{
 			jQuery('#pmpro_vat_table').show();
-			jQuery('#vat_number_validation_tr').hide();
+			
+			jQuery('#eu_self_id_instructions_tr').hide();
 			if(jQuery('#eucountry').val() == '' && jQuery('select[name=bcountry]').val() != '')
 				jQuery('#eucountry').val(jQuery('select[name=bcountry]').val());
+			
+			//Only show the self identify box if IP Geolocation & Billing address don't match
+			if(country_by_ip == country)
+				jQuery('#vat_confirm_country').hide();
+			else
+				jQuery('#vat_confirm_country').show();
 		}
 		else
 		{
@@ -33,22 +52,31 @@ jQuery(document).ready(function(){
 				data: "action=pmprovat_vat_verification_ajax_callback&country=" + country + "&vat_number=" + vat_number,
 				error: function(xml){					
 					alert('Error verifying VAT [2]');
+					jQuery("#reg_price").hide();
+					jQuery("#vat_price").show();
 					},
 				success: function(responseHTML)
 				{
 					if(responseHTML.trim() == 'true')
 					{
-						//alert("VAT Number was verified");
-						//print message
 						jQuery('#pmpro_message, #vat_number_message').show();							
 						jQuery('#pmpro_message, #vat_number_message').addClass('pmpro_success');
-						jQuery('#pmpro_message, #vat_number_message').html('VAT number was verifed');
+						jQuery('#pmpro_message, #vat_number_message').html('VAT number was verifed. You will not be charged a VAT');
+						jQuery("#reg_price").show();
+						jQuery("#vat_price").hide();
 
 						jQuery('<input>').attr({
 							type: 'hidden',
 							id: 'vat_number_verified',
 							name: 'vat_number_verified',
 							value: '1'
+						}).appendTo('#pmpro_form');
+						
+						jQuery('<input>').attr({
+							type: 'hidden',
+							id: 'vat_number',
+							name: 'vat_number',
+							value: vat_number
 						}).appendTo('#pmpro_form');
 					}
 					else
@@ -57,6 +85,8 @@ jQuery(document).ready(function(){
 						jQuery('#pmpro_message, #vat_number_message').removeClass('pmpro_success');
 						jQuery('#pmpro_message, #vat_number_message').addClass('pmpro_error');
 						jQuery('#pmpro_message, #vat_number_message').html('VAT number was not verifed. Please try again.');
+						jQuery("#reg_price").hide();
+						jQuery("#vat_price").show();
 					}
 				}
 			});
@@ -75,6 +105,13 @@ jQuery(document).ready(function(){
 			jQuery('#vat_number_validation_tr').hide();
 			jQuery('#pmpro_vat_table').focus();
 		}
+	}
+	
+	//Taken from: http://snipplr.com/view/26662/get-url-parameters-with-jquery--improved/
+	function get_query_var(name) {
+		var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href);
+		if (!results) { return 0; }
+		return results[1] || 0;
 	}
 	
 	//toggle when checking
--- pmpro-vat-tax.php
+++ pmpro-vat-tax.php
@@ -3,7 +3,7 @@
 Plugin Name: Paid Memberships Pro - VAT Tax
 Plugin URI: http://www.paidmembershipspro.com/wp/pmpro-vat-tax/
 Description: Calculate VAT tax at checkout and allow customers with a VAT Number lookup for VAT tax exemptions in EU countries.
-Version: .2
+Version: .3
 Author: Stranger Studios
 Author URI: http://www.strangerstudios.com
 */
@@ -85,7 +85,7 @@ function pmprovat_init()
 							"SK"  => "Slovakia",
 							"FI"  => "Finland",
 							"SE"  => "Sweden",
-							"UK"  => "United Kingdom"    
+							"UK"  => "United Kingdom",
 						    );
 	
 	/**
@@ -168,19 +168,6 @@ function pmprovat_verify_vat_number($country, $vat_number)
 }
 
 /**
- * Show VAT tax info below level cost text.
- */
-function pmprovat_pmpro_level_cost_text($cost, $level)
-{
-	global $pmpro_pages;
-	if( is_page( $pmpro_pages["checkout"] ) )
-		$cost .= " " . __("Members in the EU will be charged a VAT tax.", "pmprovat");
-	
-	return $cost;
-}
-add_filter("pmpro_level_cost_text", "pmprovat_pmpro_level_cost_text", 10, 2);
-
-/**
  * Show VAT country and number field at checkout.
  */
 function pmprovat_pmpro_checkout_boxes()
@@ -220,6 +207,7 @@ function pmprovat_pmpro_checkout_boxes()
 				<?php //Hidden field to enable tax?>
 				
 				<input type="hidden" id="taxregion" name="taxregion" value="1">
+				<input type="hidden" id="geo_ip" name="geo_ip" value=<?php echo pmprovat_determine_country_from_ip(); ?>>
 			</div>
 		</td>
 	</tr>
@@ -263,6 +251,38 @@ function pmprovat_vat_verification_ajax_callback()
 	exit();
 }
 
+function pmprovat_pmpro_paypal_express_return_url_parameters($params)
+{	
+	if (!empty($_REQUEST['vat_number_verified']))
+	{
+        $params["vat_number_verified"] = isset($_REQUEST['vat_number_verified']) ? intval($_REQUEST['vat_number_verified']) : null;
+        $params["vat_number"] = isset($_REQUEST['vat_number']) ? intval($_REQUEST['vat_number']) : null;
+	}
+	
+	$params["bcountry"] = isset($_REQUEST['bcountry']) ? $_REQUEST['bcountry'] : null;
+	$params["eucountry"] = isset($_REQUEST['eucountry']) ? $_REQUEST['eucountry'] : null;
+	$params["self_identify"] = isset($_REQUEST['self_identify']) ? $_REQUEST['self_identify'] : null;
+
+	return $params;	
+}
+
+add_filter('pmpro_paypal_express_return_url_parameters', 'pmprovat_pmpro_paypal_express_return_url_parameters');
+
+function pmprovat_pmpro_paypal_standard_nvpstr($str)
+{
+	if (!empty($_REQUEST['vat_number_verified']))
+	{
+		$vat_number = $_REQUEST['vat_number'];
+		$vat_number_verified = $_REQUEST['vat_number_verified'];
+		
+		$str = $str."&vat_number=".$vat_number."&vat_number_verified=".$vat_number_verified;
+	}
+
+	return $str;
+}
+
+add_filter('pmpro_paypal_standard_nvpstr', 'pmprovat_pmpro_paypal_standard_nvpstr');
+
 /**
  * Check self identified country with billing address country and verify VAT number
  */
@@ -280,14 +300,24 @@ function pmprovat_check_vat_fields_submission($value)
 	else
 		$eucountry = "";
 	
+	if(!empty($_REQUEST['geo_ip']))
+		$country_by_ip = $_REQUEST['geo_ip'];
+	else
+		$country_by_ip = '';
+	
 	//only if billing country is an EU country
 	if(!empty($bcountry) && array_key_exists($bcountry, $pmpro_european_union))
 	{
-		if($bcountry !== $eucountry)
+		if($country_by_ip != $bcountry)
 		{
-			$pmpro_msg = "Billing country and country self identification must match";
-			$pmpro_msgt = "pmpro_error";
-			$value = false;	
+			//The IP Geolocation is disagreeing with the Billing Address & the self
+			//identifcation is incorrect.
+			if($bcountry !== $eucountry)
+			{
+				$pmpro_msg = "Billing country and country self identification must match";
+				$pmpro_msgt = "pmpro_error";
+				$value = false;	
+			}
 		}
 	}
 	
@@ -340,6 +370,12 @@ function pmprovat_region_tax_check()
 	{
 		add_filter("pmpro_tax", "pmprovat_pmpro_tax", 10, 3);
 	}
+	
+	if(isset($_REQUEST['vat_number']) && isset($_REQUEST['vat_number_verified']))
+	{
+		$_SESSION['vat_number']	= $_REQUEST['vat_number'];
+		$_SESSION['vat_number_verified']	= $_REQUEST['vat_number_verified'];
+	}
 }
 add_action("init", "pmprovat_region_tax_check");
 
@@ -348,7 +384,7 @@ add_action("init", "pmprovat_region_tax_check");
  */
 function pmprovat_pmpro_tax($tax, $values, $order)
 {  	
-	global $pmpro_vat_by_country;
+	global $current_user, $pmpro_vat_by_country;
 
 	if(!empty($_REQUEST['vat_number']))
 		$vat_number = $_REQUEST['vat_number'];
@@ -382,6 +418,9 @@ function pmprovat_pmpro_tax($tax, $values, $order)
 		if(!empty($vat_number) && !empty($eucountry) && pmprovat_verify_vat_number($eucountry, $vat_number))
 		{
 			$vat_rate = 0;
+			
+			//set this so when we return from PayPal Express (w/extra step) we will show the non-VAT price.
+			$_REQUEST['vat_number_verified'] = true;
 		}
 		//they don't have a VAT number.
 		elseif(!empty($eucountry) && array_key_exists($eucountry, $pmpro_vat_by_country))
@@ -405,20 +444,223 @@ function pmprovat_pmpro_tax($tax, $values, $order)
 	}
 
 	if(!empty($vat_rate))
-		$tax = $tax + round((float)$values['price'] * $vat_rate, 2);
+		$tax = $tax + pmprovat_calculate_vat($values['price'], $vat_rate, $eucountry);
 
 	return $tax;
+}
+
+function pmprovat_calculate_vat($price, $rate, $country)
+{
+	$tax = round((float)$price * $rate, 2);
+	
+	//Use the filter for custom rounding rules
+	$tax = apply_filters('pmprovat_calculate_custom_vat', $tax, $price, $rate, $country);
+	
+	return $tax;
+}
+
+function pmprovat_get_vat_rate($country, $state = false)
+{
+	if($country == 'GB')
+		$country = 'UK';
+	
+	if($country == 'GR')
+		$country = 'EL';
+	
+	global $pmpro_vat_by_country;
+	
+	if(!$state)
+		$vat_rate = $pmpro_vat_by_country[$country];
+	
+	return $vat_rate;
 }
 
 /**
  * Remove the taxregion session var on checkout
  */
 function pmprovat_pmpro_after_checkout()
-{
+{		
+	if(isset($_SESSION['vat_number']))
+		unset($_SESSION['vat_number']);
+		
+	if(isset($_SESSION['vat_number_verified']))
+		unset($_SESSION['vat_number_verified']);
+	
 	if(isset($_SESSION['taxregion']))
 		unset($_SESSION['taxregion']);
 }
 add_action("pmpro_after_checkout", "pmprovat_pmpro_after_checkout");
+add_action('pmpro_before_send_to_paypal_standard', 'pmprovat_pmpro_after_checkout', 10);
+
+//Add to admin order pages
+function pmprovat_pmpro_add_cols_header($order_ids)
+{
+	global $pmpro_currency;?>
+
+	<th>VAT Verification</th>
+	<th>VAT Number</th>
+	<th>IP</th>
+	<th>Database</th>
+	<th>Percentage</th>
+	<th>Tax</th>
+	<th>Country</th>
+	<th>Conversion Rate ( <?php echo $pmpro_currency. ' to EURO'; ?>) </th><?php
+}
+add_filter('pmpro_orders_extra_cols_header', 'pmprovat_pmpro_add_cols_header');
+
+function pmprovat_pmpro_add_cols_body($order)
+{
+	global $pmpro_currency_symbol;
+	
+	$vat_method 			= pmpro_getMatches("/{EU_VAT_VERIFY_SOURCE:([^}]*)}/", $order->notes, true);
+	$vat_percent 			= pmpro_getMatches("/{EU_VAT_PERCENT:([^}]*)}/", $order->notes, true);
+	$vat_geo_ip_database 	= pmpro_getMatches("/{EU_VAT_GEO_IP_DATABASE:([^}]*)}/", $order->notes, true);
+	$vat_geo_ip 			= pmpro_getMatches("/{EU_VAT_GEO_IP:([^}]*)}/", $order->notes, true);
+	$vat_country 			= pmpro_getMatches("/{EU_VAT_COUNTRY:([^}]*)}/", $order->notes, true);
+	$conversion_rate		= pmpro_getMatches("/{EU_VAT_EURO_RATE:([^}]*)}/", $order->notes, true);
+	$vat_number			= pmpro_getMatches("/{EU_VAT_NUMBER:([^}]*)}/", $order->notes, true);
+		
+	if(!empty($vat_method))	
+		echo '<td>'.$vat_method.'</td>';
+	else
+		echo '<td></td>';
+	
+	if(!empty($vat_number))	
+		echo '<td>'.$vat_number.'</td>';
+	else
+		echo '<td></td>';
+	
+	if(!empty($vat_geo_ip))
+		echo '<td>'.$vat_geo_ip.'</td>';
+	else
+		echo '<td></td>';
+	
+	if(!empty($vat_geo_ip_database))
+		echo '<td>'.$vat_geo_ip_database.'</td>';
+	else
+		echo '<td></td>';
+	
+	if(!empty($vat_percent))
+		echo '<td>'.$vat_percent.'</td>';
+	else
+		echo '<td></td>';
+	
+	if(!empty($order->tax))
+		echo '<td>'.$pmpro_currency_symbol.$order->tax.'</td>';
+	else
+		echo '<td></td>';
+	
+	if(!empty($vat_country))
+		echo '<td>'.$vat_country.'</td>';
+	else
+		echo '<td></td>';
+	
+	if(!empty($conversion_rate))
+		echo '<td>'.$conversion_rate.'</td>';
+	else
+		echo '<td></td>';	
+}
+add_filter('pmpro_orders_extra_cols_body', 'pmprovat_pmpro_add_cols_body');
+
+function pmprovat_pmpro_invoice_bullets_bottom($pmpro_invoice)
+{
+	global $pmpro_currency_symbol;
+	
+	$vat_percent	= pmpro_getMatches("/{EU_VAT_PERCENT:([^}]*)}/", $pmpro_invoice->notes, true);
+	$vat_number	= pmpro_getMatches("/{EU_VAT_NUMBER:([^}]*)}/", $pmpro_invoice->notes, true);
+
+	if(isset($vat_percent) && $vat_percent > 0)
+		echo '<li><strong>VAT Rate: </strong>'.$vat_percent*100 .'%</li>';
+	elseif(isset($vat_number))
+		echo '<li><strong>VAT Number: </strong>'.$vat_number.'</li>';
+}
+
+add_action('pmpro_invoice_bullets_bottom', 'pmprovat_pmpro_invoice_bullets_bottom');
+
+function pmprovat_pmpro_orders_csv_extra_columns($columns)
+{
+	$columns["vat_method"]		= "pmprovat_extra_column_vat_method";
+	$columns["vat_number"]		= "pmprovat_extra_column_vat_number";
+	$columns["percent"]			= "pmprovat_extra_column_vat_percent";
+	$columns["ip"]				= "pmprovat_extra_column_vat_ip";
+	$columns["source"]			= "pmprovat_extra_column_vat_source";
+	$columns["conversion_rate"]	= "pmprovat_extra_column_vat_conversion_rate";
+
+	return $columns;
+}
+add_filter("pmpro_orders_csv_extra_columns", "pmprovat_pmpro_orders_csv_extra_columns", 10);
+
+function pmprovat_extra_column_vat_method($order)
+{
+	$vat_method = pmpro_getMatches("/{EU_VAT_VERIFY_SOURCE:([^}]*)}/", $order->notes, true);
+	
+	if($vat_method)
+	{
+		return $vat_method;
+	}
+	else
+		return '';
+}
+
+function pmprovat_extra_column_vat_number($order)
+{
+	$vat_number = pmpro_getMatches("/{EU_VAT_NUMBER:([^}]*)}/", $order->notes, true);
+	
+	if($vat_number)
+	{
+		return $vat_number;
+	}
+	else
+		return '';
+}
+
+function pmprovat_extra_column_vat_ip($order)
+{
+	$vat_geo_ip = pmpro_getMatches("/{EU_VAT_GEO_IP:([^}]*)}/", $order->notes, true);
+	
+	if($vat_geo_ip)
+	{
+		return $vat_geo_ip;
+	}
+	else
+		return '';
+}
+
+function pmprovat_extra_column_vat_source($order)
+{
+	$vat_geo_ip_database = pmpro_getMatches("/{EU_VAT_GEO_IP_DATABASE:([^}]*)}/", $order->notes, true);
+	
+	if($vat_geo_ip_database)
+	{
+		return $vat_geo_ip_database;
+	}
+	else
+		return '';	
+}
+
+function pmprovat_extra_column_vat_percent($order)
+{
+	$vat_percent = pmpro_getMatches("/{EU_VAT_PERCENT:([^}]*)}/", $order->notes, true);
+
+	if($vat_percent)
+	{
+		return $vat_percent;
+	}
+	else
+		return '';	
+	
+}
+
+function pmprovat_extra_column_vat_conversion_rate($order)
+{
+	$conversion_rate = pmpro_getMatches("/{EU_VAT_EURO_RATE:([^}]*)}/", $order->notes, true);
+	
+	if($conversion_rate)
+		return $conversion_rate;
+	else
+		return '';
+}
+
 
 /**
  * Function to add links to the plugin row meta
@@ -435,3 +677,186 @@ function pmprovat_plugin_row_meta($links, $file) {
 	return $links;
 }
 add_filter('plugin_row_meta', 'pmprovat_plugin_row_meta', 10, 2);
+
+function pmprovat_determine_country_from_ip()
+{
+	global $country_from_ip;
+	
+	//check if the GEO IP Detect plugin is active
+	if(!defined('GEOIP_DETECT_VERSION'))
+		return false;
+	
+	if(!isset($country_from_ip))
+	{
+		//get the country
+		$record = geoip_detect2_get_info_from_current_ip();
+		$country_from_ip = $record->country->isoCode;
+
+		if(empty($country_from_ip))
+			$country_from_ip = false;
+	}
+	
+	return $country_from_ip;
+}
+
+/**
+ * Show VAT tax info below level cost text.
+ */
+function pmprovat_pmpro_level_cost_text($r, $level, $tags, $short)
+{	
+	if(isset($_REQUEST['self_identify']))
+		$self_identify = $_REQUEST['self_identify'];
+	else
+		$self_identify = 0;
+	
+	if(isset($_REQUEST['bcountry']))
+		$bcountry = $_REQUEST['bcountry'];
+	else
+		$bcountry = 0;
+	
+	global $already_run, $country_from_ip, $pmpro_vat_by_country, $pmpro_european_union, $pmpro_pages, $post;
+	if(!is_array($already_run))
+		$already_run = array();
+	
+	if(!isset($already_run[$level->id]) && !is_admin())
+	{
+		$already_run = array();
+	
+		$country_from_ip = pmprovat_determine_country_from_ip();
+	
+		//we are in the EU
+		if($country_from_ip && array_key_exists($country_from_ip, $pmpro_european_union))
+		{	
+			$vat_rate = $pmpro_vat_by_country[$country_from_ip];
+			
+			if($self_identify == 1)
+				$vat_rate = $pmpro_vat_by_country[$bcountry];
+		
+			if(!empty($vat_rate))
+			{
+				$level = pmprovat_pmpro_apply_vat_to_level($level, $vat_rate);
+			}
+		}
+		
+		$already_run[$level->id] = true;
+		
+		if($pmpro_pages['checkout'] == $post->ID)
+			return '<span id = "reg_price">'.$r.'</span><span id = "vat_price">'.pmpro_getLevelCost($level, $tags, $short).'</span>';
+		else return pmpro_getLevelCost($level, $tags, $short);
+	}
+	
+	else
+	{
+		return $r;
+	}
+}
+
+add_filter('pmpro_level_cost_text', 'pmprovat_pmpro_level_cost_text', 10, 4);
+
+function pmprovat_pmpro_apply_vat_to_level($level, $vat_rate)
+{
+	$level->initial_payment = $level->initial_payment * (1 + $vat_rate);
+	$level->trial_amount = $level->trial_amount * (1 + $vat_rate);
+	$level->billing_amount = $level->billing_amount * (1 + $vat_rate);
+	
+	return $level;
+}
+
+function pmprovat_init_load_session_vars($params)
+{
+	if(empty($_REQUEST['vat_number_verified']) && !empty($_SESSION['vat_number_verified']))
+	{
+		$_REQUEST['vat_number_verified'] = $_SESSION['vat_number_verified'];
+		$_REQUEST['vat_number'] = $_SESSION['vat_number'];
+	}
+	
+	return $params;
+}
+
+add_action('init', 'pmprovat_init_load_session_vars', 5);
+
+//add note with vat data
+function pmprovat_pmpro_added_order($order)
+{
+	global $pmpro_currency, $wpdb;
+	
+	if(isset($_REQUEST['geo_ip']))
+		$country_by_ip = $_REQUEST['geo_ip'];
+	
+	if(isset($_REQUEST['bcountry']))
+		$bcountry = $_REQUEST['bcountry'];
+	
+	if(isset($_REQUEST['eucountry']))
+		$eucountry = $_REQUEST['eucountry'];
+	
+	if(isset($_REQUEST['vat_number']))
+		$vat_number = $_REQUEST['vat_number'];
+	
+	if(isset($_REQUEST['vat_number_verified']))
+		$vat_number_verified = $_REQUEST['vat_number_verified'];
+	
+	$euro_conversion_rate = pmprovat_get_euro_conversion_rate($pmpro_currency);
+	
+	$notes = "";
+		
+	//vat number	
+	if(!empty($vat_number) && $vat_number_verified)
+	{
+		//they used VAT Validation
+		$notes .= "\n---\n{EU_VAT_VERIFY_SOURCE: VAT_NUMBER" . "}\n---\n";
+		$notes .= "\n---\n{EU_VAT_NUMBER:" . $vat_number . "}\n---\n";
+		$notes .= "\n---\n{EU_VAT_COUNTRY:" . $bcountry . "}\n---\n";
+	}
+	
+	//geo ip was used
+	elseif($bcountry == $country_by_ip)
+	{
+		$notes .= "\n---\n{EU_VAT_VERIFY_SOURCE: GEO_IP" . "}\n---\n";
+		$notes .= "\n---\n{EU_VAT_GEO_IP:" . geoip_detect2_get_client_ip() . "}\n---\n";
+		$notes .= "\n---\n{EU_VAT_GEO_IP_DATABASE:" . geoip_detect2_get_current_source_description() . "}\n---\n";
+		$notes .= "\n---\n{EU_VAT_PERCENT:" . pmprovat_get_vat_rate($bcountry) . "}\n---\n";
+		$notes .= "\n---\n{EU_VAT_COUNTRY:" . $bcountry . "}\n---\n";
+	}
+	
+	//they self identified
+	elseif($bcountry == $eucountry)
+	{
+		$notes .= "\n---\n{EU_VAT_VERIFY_SOURCE: SELF_IDENTIFY" . "}\n---\n";
+		$notes .= "\n---\n{EU_VAT_PERCENT:" . pmprovat_get_vat_rate($bcountry) . "}\n---\n";
+		$notes .= "\n---\n{EU_VAT_COUNTRY:" . $bcountry . "}\n---\n";
+	}
+
+	if($euro_conversion_rate !== false)
+	{
+		$notes .= "\n---\n{EU_VAT_EURO_RATE:" . $euro_conversion_rate . "}\n---\n";
+	}	
+	
+	//add conversion rate to note
+	$order->notes .= $notes;
+	$sqlQuery = "UPDATE $wpdb->pmpro_membership_orders SET notes = '" . esc_sql($order->notes) . "' WHERE id = '" . intval($order->id) . "' LIMIT 1";
+	$wpdb->query($sqlQuery);
+	
+	return $order;
+}
+add_action('pmpro_added_order', 'pmprovat_pmpro_added_order');
+
+//get the conversion rate to the Euro
+function pmprovat_get_euro_conversion_rate($currency)
+{
+	//Adapted from:
+	//https://www.ecb.europa.eu/stats/exchange/eurofxref/html/index.en.html and
+	//https://wordpress.org/plugins/woocommerce-eu-vat-compliance/
+	
+	//the file is updated daily between 2.15 p.m. and 3.00 p.m. CET
+	$XML = simplexml_load_file("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
+	
+	foreach($XML->Cube->Cube->Cube as $cur)
+	{
+		if (isset($cur['currency']) && $currency == $cur['currency'] && isset($cur['rate']))
+		{
+			return (float)$cur['rate'];
+		}
+	}
+
+	return false;
+}
